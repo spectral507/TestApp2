3cea5b9646d59d701ee657628fa657d56ec81ae1
spectral507 <spectral507@yandex.ru>
2018-10-24 19:24:52 +0300

    init commit

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,17 @@
+const path = require('path');
+
 module.exports = {
-  lintOnSave: false
-}
+    lintOnSave: false,
+    configureWebpack: config => {
+        config.output.path = path.join(__dirname, 'wwwroot/dist');
+        config.resolve.alias['@'] = 'C:\\Projects\\Vuejs\\TestApp2\\ClientApp';
+        config.entry.app = './ClientApp/main.js';
+        let jsxRule = config.module.rules.find(rule => rule.test.test('*.jsx'));
+        if (jsxRule) {
+            jsxRule.include[0] = 'C:\\Projects\\Vuejs\\TestApp2\\ClientApp';
+        }
+    },
+    chainWebpack: cfg => {
+        cfg.plugins.delete('html');
+    }
+};
